c8440a63f4d46bcd8e87d64c3372cb2e19ea036d
David Su <david.d.su@gmail.com>
2016-05-01 02:33:32 -0400

    implement basic chord construction from clientNotes

--- src/server/PlayerExperience.js
+++ src/server/PlayerExperience.js
@@ -12,6 +12,7 @@ export default class PlayerExperience extends Experience {
     super(clientType);
 
     this.checkin = this.require('checkin');
+    this.clientNotes = {};
   }
 
   /**
@@ -25,7 +26,16 @@ export default class PlayerExperience extends Experience {
     this.broadcast(client.type, client, 'play');
 
     this.receive(client, 'midinote', (midinote) => {
-      console.log(midinote);
+      console.log("received note " + midinote + " from client " + client.uuid + " (" + Object.keys(client) + ")");
+
+      this.clientNotes[client.uuid] = midinote;
+      console.log(this.clientNotes);
+
+      // flesh out chords
+      var chord = this.createChord(this.clientNotes);
+      var clientNotes = this.clientNotes;
+      console.log("constructed chord " + chord + " from aggregated clientNotes " + Object.keys(this.clientNotes).map(function(k) { return clientNotes[k]; }));
+
     });
   }
 
@@ -33,6 +43,37 @@ export default class PlayerExperience extends Experience {
     super.exit(client);
     // ...
   }
+
+  createChord(clientNotes) {
+    var minChordSize = 3;
+    var notes = [];
+    for (var k in clientNotes) {
+      notes.push(clientNotes[k]);
+    }
+
+    // method 1: calculate dissonance using lg(LCM(x, y)) for each note
+
+    // method 2: fourths, fifths, octaves
+    var intervals = [5, 7, 12];
+
+    while (notes.length < minChordSize) {
+      var newNotes = [];
+      for (var i=0; i<notes.length; i++) {
+        var note = notes[i];
+        var intervalIdx = Math.floor(Math.random() * intervals.length);
+        var newInterval = intervals[intervalIdx];
+        var newNote = note + newInterval;
+        newNotes.push(newNote);
+      }
+
+      for (var i=0; i<newNotes.length; i++) {
+        notes.push(newNotes[i]);
+      }
+    }
+
+    return notes;
+  }
+
 
   /**
    * If anything needs to happen when a client connects to the server,
